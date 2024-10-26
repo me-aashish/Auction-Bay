49538f284a3c0483a0101408440060027e1e0e28
harshit <harshit.kumar@effigoglobal.com>
2024-10-26 14:15:59 +0530

    initial commit

---
+++ src/main/java/com/auction/bay/auction_bay/AuctionBayApplication.java
@@ -0,0 +1,13 @@
+package com.auction.bay.auction_bay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuctionBayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuctionBayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/auction/bay/auction_bay/AuctionBayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.auction.bay.auction_bay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuctionBayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
